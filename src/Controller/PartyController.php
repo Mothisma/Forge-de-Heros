b5d85aa3682b65b82c62f7d8a35e8dbfcb5a34ba
Mathis GILGENKRANTZ <157127725+Mothisma@users.noreply.github.com>
2026-03-23 13:44:56 +0100

    debut edit party

--- src/Controller/PartyController.php
+++ src/Controller/PartyController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Character;
 use App\Entity\Party;
 use App\Form\PartyType;
+use App\Repository\CharacterRepository;
 use App\Repository\PartyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -49,6 +51,35 @@ final class PartyController extends AbstractController
         return $this->render('party/show.html.twig', [
             'party' => $party,
         ]);
+    }
+
+    #[Route('/{id}/add',name: 'app_party_add', methods: ['GET'])]
+    public function add(Party $party, CharacterRepository $characterRepository): Response
+    {
+        $characterCount = 0;
+        foreach ($party->getCharacters() as $character) {
+            $characterCount++;
+        }
+        if ($characterCount >= $party->getMaxSize()) {
+            return $this->redirectToRoute('app_party_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        $availableCharacters = array_filter(
+            $characterRepository->findBy(['user'=> $this->getUser()]),
+            fn ($character)=> !$party->getCharacters()->contains($character)
+        );
+
+        return $this->render('party/add.html.twig', [
+            'party' => $party,
+            'availableCharacters' => $availableCharacters,
+        ]);
+    }
+
+    #[Route('/{id}/add/{character_id}',name: 'app_party_add', methods: ['POST'])]
+    public function addCharacter(Request $request, EntityManagerInterface $entityManager, CharacterRepository $characterRepository): Response
+    {
+        $characterCount = 0;
+
     }
 
 }
